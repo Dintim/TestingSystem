cb073707e9034a00d46eb58c374da50c64ff28bb
dm <muk_dinara@mail.ru>
2020-02-17 01:42:54 +0600

    Добавьте файлы проекта.

---
+++ TestingSystem/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TestingSystem.Pages;
+
+namespace TestingSystem
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Frame _frame = new Frame();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //_frame.Navigate(new PageCreateTest());            
+            NavigationService nav = NavigationService.GetNavigationService(this);
+            nav.Navigate(new PageCreateTest());            
+        }
+    }
+}
---
+++ TestingSystem/Models/Question.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingSystem.Models
+{
+    public class Question
+    {
+        public string Description { get; set; }
+        public Tuple<string, bool> Answers { get; set; }
+        public int Score { get; set; }
+    }
+}
